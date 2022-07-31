44361def576958eb7676961b613b2fb1297f36e5
LexWriter <rolleks@mail.ru>
2022-07-31 15:56:11 +0300

    trafficLights

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lightsRed: UIView!
+    @IBOutlet var lightsYellow: UIView!
+    @IBOutlet var lightsGreen: UIView!
+    @IBOutlet var buttonAction: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        lightsRed.isHidden = true
+        lightsRed.layer.cornerRadius = 60
+        lightsYellow.isHidden =  true
+        lightsYellow.layer.cornerRadius = 60
+        lightsGreen.isHidden = true
+        lightsGreen.layer.cornerRadius = 60
     }
-
-
+    @IBAction func buttonPressed() {
+        buttonAction.setTitle("Next", for: .normal)
+        
+        if lightsRed.isHidden && lightsYellow.isHidden{
+            lightsRed.isHidden = false
+            lightsGreen.isHidden = true
+        }else if lightsYellow.isHidden && lightsGreen.isHidden{
+            lightsYellow.isHidden = false
+            lightsRed.isHidden = true
+        }
+        else if lightsRed.isHidden && lightsGreen.isHidden{
+            lightsGreen.isHidden = false
+            lightsYellow.isHidden = true
+        }
+    }
 }
 
